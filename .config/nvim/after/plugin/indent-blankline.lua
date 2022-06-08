4ac62edad0877985a1164245611af0d197492cc2
cuckflong <johnathanclong13@gmail.com>
2022-06-08 22:27:59 +0800

    nvim

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,6 +1,6 @@
 require("indent_blankline").setup {
     show_current_context = true,
-    show_current_context_start = true,
+    -- show_current_context_start = true,
     show_trailing_blankline_indent = false,
     space_char_blankline = " ",
 }
